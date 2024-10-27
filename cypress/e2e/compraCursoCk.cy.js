16a4ad6b9c3e8c4c21408449c71dc1c2426dab47
TFthiago <tfreitasto21@gmail.com>
2024-10-27 10:33:09 -0300

    Ajustes2

--- cypress/e2e/compraCursoCk.cy.js
+++ cypress/e2e/compraCursoCk.cy.js
@@ -1,8 +1,8 @@
 describe('Fluxo para compra de curso - Plataforma testando Eveclass', () => {
        
     before(() => {
-        //Login com memória de Cookie
-        cy.setCookie('ev_at_62bf145fd8ff1ef247cd4eda', '18016fd0-93d1-11ef-a3b4-1595982be9f618aac7c6-dcde-4ffb-a2c8-4a42d0e51c8e')
+        //Login com Cookie
+        cy.setCookie('ev_at_62bf145fd8ff1ef247cd4eda', 'a0ecd970-9461-11ef-8c2a-e1b8a98d0262d7597506-1611-409b-a6e6-da0c1efa711e')
 
         //Visitar página de perfil
         cy.visit('/conta/meus-cursos')
@@ -11,20 +11,26 @@ describe('Fluxo para compra de curso - Plataforma testando Eveclass', () => {
     it('Realização da compra', () => {
 
         //Página de perfil (Meus cursos)//
+
+        //Botão 'Procurar cursos' 
         cy.get('.button-default')
             .should('contain.text', ' \n        Procurar cursos\n       ')
             .eq(0)
             .click()
         
+
         //Página catálogo de cursos//
 
+        //Verificação do título
         cy.get('h1.course-list-nav')
             .should('contain.text', 'Todos cursos')
 
+        //Primeiro link / banner da página
         cy.get('[class="content-card-name variant-primary "]')
             .should('be.visible')
             .eq(0)
             .click()
+
 
         //Página do curso//
         
@@ -32,39 +38,36 @@ describe('Fluxo para compra de curso - Plataforma testando Eveclass', () => {
         cy.get('.content-price')
             .eq(0)
             .contains(/^R\$ 0,00$/)
-                               
+                            
+        //Botão comprar
         cy.get('.content-action')
             .should('be.visible')
             .eq(0)
             .click()
 
-        //Prenchimento do formulário
-        cy.get('[data-vv-as="CPF"]')
-            .should('be.visible')
-            .click()
-            .type('96680716039')
-        
-        cy.get('[data-vv-as="Telefone"]')
-            .should('be.visible')
-            .click()
-            .type('55555555555')
-        
-        cy.get('[data-vv-as="CEP"]')
-            .should('be.visible')
-            .click()
-            .type('07111050')
 
+        //Prenchimento do formulário//
+
+        //Inserir CPF
+        cy.getActElement('[data-vv-as="CPF"]', '96680716039' )
+
+        //Inserir telefone
+        cy.getActElement('[data-vv-as="Telefone"]', '55555555555' )
+
+        //Inserir CEP
+        cy.getActElement('[data-vv-as="CEP"]', '07111050' )
+
+        //Inserir complemento / número
         cy.get('[data-vv-as="Número"]')
             .should('be.visible')
             .click()
+            .clear()
             .type('13')
 
         //Confirmar compra
-        // cy.get('.button-text')
-        //     .contains(/^Confirmar Compra$/)
-        //     .click()
+        cy.get('.button-text')
+            .contains(/^Confirmar Compra$/)
+            .click()
 
-        //Resolver captcha de segurança
-        // cy.resolveCaptcha()
     })
 })
--- cypress/e2e/criacaoCadastro.cy.js
+++ cypress/e2e/criacaoCadastro.cy.js
@@ -1,29 +1,32 @@
 describe('Teste de criação de conta - Plataforma testando Eveclass', () => {
     
     before(() => {
-    //Página de cadastro
+    //Visitar Página de cadastro
         cy.visit('/auth/registrar')
     })
 
     it('Teste cadastro de conta', () => {
         
         //Inserir nome completo
-        cy.get('[data-vv-as="Seu nome completo"]')
-            .should('be.visible')
-            .click()
-            .type('Usuario442')
+        cy.getActElement('[data-vv-as="Seu nome completo"]', 'Usuario023')
+        // cy.get('[data-vv-as="Seu nome completo"]')
+        //     .should('be.visible')
+        //     .click()
+        //     .type('Usuario442')
 
         //Inserir email
-        cy.get('[data-vv-as="Seu email"]')
-            .should('be.visible')
-            .click()
-            .type('testerqa6@yopmail.com')
+        cy.getActElement('[data-vv-as="Seu email"]', 'testerqa8@yopmail.com')
+        // cy.get('[data-vv-as="Seu email"]')
+        //     .should('be.visible')
+        //     .click()
+        //     .type('testerqa6@yopmail.com')
 
         //Confirmar email
-        cy.get('[data-vv-as="Confirme seu email"]')
-            .should('be.visible')
-            .click()
-            .type('testerqa6@yopmail.com')
+        cy.getActElement('[data-vv-as="Confirme seu email"]', 'testerqa8@yopmail.com')
+        // cy.get('[data-vv-as="Confirme seu email"]')
+        //     .should('be.visible')
+        //     .click()
+        //     .type('testerqa6@yopmail.com')
         
         //Botão 'Próximo'
         cy.get('.button-text')
@@ -35,16 +38,18 @@ describe('Teste de criação de conta - Plataforma testando Eveclass', () => {
         // cy.resolveCaptcha()
 
         //Inserir senha
-        cy.get('[data-vv-as="Sua senha"]')
-            .should('be.visible')
-            .click()
-            .type('123456789')
+        cy.getActElement('[data-vv-as="Sua senha"]', '123456789')
+        // cy.get('[data-vv-as="Sua senha"]')
+        //     .should('be.visible')
+        //     .click()
+        //     .type('123456789')
 
         //Confirmar senha
-        cy.get('[data-vv-as="Confirme sua senha"]')
-            .should('be.visible')
-            .click()
-            .type('123456789')
+        cy.getActElement('[data-vv-as="Confirme sua senha"]', '123456789')
+        // cy.get('[data-vv-as="Confirme sua senha"]')
+        //     .should('be.visible')
+        //     .click()
+        //     .type('123456789')
 
         //Botão 'Criar conta'
         cy.get('.button-default')
